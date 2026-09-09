15651ce11c339cf33783fb598dcc195a3b48d2ef
kayque001-cyber <okayque363@gmail.com>
2026-09-09 16:49:51 -0300

    AT_04

--- Central/views.py
+++ Central/views.py
@@ -9,6 +9,18 @@ def inicio(request):
 def lista_livros(request):
     livros = Livro.objects.all()
 
+    busca = request.GET.get('busca')
+
+    if busca:
+        livros = livros.filter(
+            titulo__icontains=busca
+        ) | livros.filter(
+            autor__icontains=busca
+        ) | livros.filter(
+            categoria__icontains=busca
+        )
+
     return render(request, 'pages/livros.html', {
-        'livros': livros
+        'livros': livros,
+        'busca': busca,
     })
